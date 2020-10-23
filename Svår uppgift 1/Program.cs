ea0fd27ac25fa2956c6d5783ba5d440e3850951c
Bruckis <noah.bruckmann@gmail.com>
2020-10-23 05:37:19 +0000

    Add files via upload

---
+++ Svår uppgift 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Svår_uppgift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett tal");
+            int tal = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= tal; i++) // Denna kommer generera alla rader
+            {
+                int starsperrow = i; // Hur många stjärnor det kommer vara per rad
+                for (int p = 0; p < tal - (i * 2 -1); p++)
+                {
+                    Console.Write(" ");
+                }
+                for (int n = 0; n < starsperrow; n++) // Skriver ut stjärnorna på varje rad
+                {
+                    if (n == starsperrow - 1) // För att få en ny rad när vi har skrivit ut tillräckligt med stjärnor
+                    {
+                        Console.WriteLine("* ");
+                    }
+                    else
+                    {
+                        Console.Write("* ");
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Svår uppgift 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Svår_uppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ditt saldo");
+            double saldo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Skriv räntan i procentenheter utan tecknet");
+            double ränta = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Skriv ditt slutmål");
+            double slutmål = Convert.ToDouble(Console.ReadLine());
+            double years = 0;
+            ränta = ränta / 100;
+            for (double i = saldo; i < slutmål; i = saldo)
+            {
+                double förändring = saldo * ränta;
+                saldo = saldo + förändring;
+                years++;
+                Console.WriteLine("Efter " + years + " år kommer du ha " + saldo);
+                
+            }
+            Console.WriteLine("Det kommer ta " + years + " år innan du har nått ditt slutmål");
+            Console.ReadLine();
+
+        }
+    }
+}
